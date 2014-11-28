4b6d71d84c464a06cf99abd6a60cea336a089678
jmks <jason.schweier@gmail.com>
2014-11-28 09:15:18 -0500

    implemented states and updated TennisGame to use them

--- lib/tennis2.rb
+++ lib/tennis2.rb
@@ -4,14 +4,6 @@ Player = Struct.new :name, :points
 
 class TennisGame
 
-  SCORE_IN_WORDS = {
-    0 => "Love",
-    1 => "Fifteen",
-    2 => "Thirty",
-    3 => "Forty",
-    4 => "" # placeholder
-  }
-
   def initialize(player1Name, player2Name)
     @player1 = Player.new(player1Name, 0)
     @player2 = Player.new(player2Name, 0)
@@ -21,73 +13,15 @@ class TennisGame
   def won_point(playerName)
     if playerName == @player1.name
       p1Score()
+      @state = @state.transition!(@player1)
     else
       p2Score()
+      @state = @state.transition!(@player2)
     end
   end
 
   def say_score
-    result = ""
-    
-    if @player1.points == @player2.points && @player1.points < 3
-      result = SCORE_IN_WORDS[@player1.points] + "-All"
-    end
-    
-    if @player1.points == @player2.points && @player1.points > 2
-        result = "Deuce"
-    end
-    
-    p1res = p2res = "Love"
-    if @player1.points > 0 && @player2.points.zero?
-      # p1res needed below (overlapping conditions)
-      p1res = SCORE_IN_WORDS[@player1.points]
-      result = p1res + "-" + p2res
-    end
-
-    if @player2.points > 0 && @player1.points.zero?
-      p2res = SCORE_IN_WORDS[@player2.points]
-      result = p1res + "-" + p2res
-    end
-    
-    if @player1.points > @player2.points && @player1.points < 4
-      if [2, 3].include? @player1.points
-        p1res = SCORE_IN_WORDS[@player1.points]
-      end
-
-      if [1,2].include? @player2.points
-        p2res = SCORE_IN_WORDS[@player2.points]
-      end
-      
-      result = p1res + "-" + p2res
-    end
-
-    if @player2.points > @player1.points && @player2.points < 4
-      if [2, 3].include? @player2.points
-        p2res = SCORE_IN_WORDS[@player2.points]
-      end
-
-      if [1,2].include? @player1.points
-        p1res = SCORE_IN_WORDS[@player1.points]
-      end
-
-      result = p1res + "-" + p2res
-    end
-
-    if @player1.points > @player2.points && @player2.points >= 3
-      result = "Advantage " + @player1.name
-    end
-    if @player2.points > @player1.points && @player1.points >= 3
-      result = "Advantage " + @player2.name
-    end
-
-    if @player1.points >= 4 && @player2.points >= 0 && (@player1.points - @player2.points) >= 2
-      result = "Win for " + @player1.name
-    end
-    if @player2.points >= 4 && @player1.points>=0 && (@player2.points - @player1.points) >= 2
-      result = "Win for " + @player2.name
-    end
-
-    result
+    @state.say_score
   end
 
   def setp1Score(number)
@@ -98,7 +32,6 @@ class TennisGame
     number.times { p2Score }
   end
 
-  # deprecated
   def p1Score
     @player1.points += 1
   end
--- lib/tennis_score_states.rb
+++ lib/tennis_score_states.rb
@@ -1,43 +1,77 @@
 class RallyState
+
+  SCORE_IN_WORDS = {
+    0 => "Love",
+    1 => "Fifteen",
+    2 => "Thirty",
+    3 => "Forty"
+  }
+
   def initialize(player1, player2)
+    @player1, @player2 = player1, player2
   end
 
   def say_score
+    if @player1.points == @player2.points
+      "#{ SCORE_IN_WORDS[@player1.points] }-All"
+    else
+      "#{ SCORE_IN_WORDS[@player1.points] }-#{ SCORE_IN_WORDS[@player2.points] }"
+    end
   end
 
   def transition!(scoring_player)
+    if @player1.points == @player2.points && scoring_player.points > 2
+      DeuceState.new(@player1, @player2)
+    elsif scoring_player.points >= 4 && (@player1.points - @player2.points).abs >= 2
+      DoneState.new(scoring_player)
+    else
+      self
+    end
   end
 end
 
 class DeuceState
   def initialize(player1, player2)
+    @player1, @player2 = player1, player2
   end
 
   def say_score
+    "Deuce"
   end
 
   def transition!(scoring_player)
+    AdvantageState.new(@player1, @player2, scoring_player)
   end
 end
 
 class AdvantageState
   def initialize(player1, player2, advantage_player)
+    @player1, @player2, @advantage_player = player1, player2, advantage_player
   end
 
   def say_score
+    "Advantage #{ @advantage_player.name }"
   end
 
   def transition!(scoring_player)
+    if scoring_player.name == @advantage_player.name
+      DoneState.new(scoring_player)
+    else
+      DeuceState.new(@player1, @player2)
+    end
   end
 end
 
 class DoneState
-  def initialize(player1, player2, winning_player)
+  def initialize(winning_player)
+    @winning_player = winning_player
   end
 
   def say_score
+    "Win for #{ @winning_player.name }"
   end
 
   def transition!(scoring_player)
+    self
   end
 end
