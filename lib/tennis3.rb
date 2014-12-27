ac6bbdd1047bf53f37ba4653fe36e31bc57f4939
jmks <jason.schweier@gmail.com>
2014-12-27 18:07:22 -0500

    added ScoreSayerVisitor. moved say_score logic to visitor.

--- lib/tennis3.rb
+++ lib/tennis3.rb
@@ -1,5 +1,5 @@
 class TennisGame
-  
+
   attr_reader :player1_name, :player2_name, :player1_score, :player2_score
 
   def initialize(player1Name, player2Name)
@@ -7,6 +7,8 @@ class TennisGame
     @player2_name  = player2Name
     @player1_score = 0
     @player2_score = 0
+
+    @default_visitor = ScoreSayerVisitor.new
   end
       
   def won_point(player_name)
@@ -17,17 +19,24 @@ class TennisGame
     end
   end
   
-  def say_score
-    if (@player1_score < 4 and @player2_score < 4) and (@player1_score + @player2_score < 6)
+  def say_score visitor=nil
+    visitor ||= @default_visitor
+    visitor.say_score(self)
+  end
+end
+
+class ScoreSayerVisitor
+  def say_score game
+    if (game.player1_score < 4 and game.player2_score < 4) and (game.player1_score + game.player2_score < 6)
       p = ["Love", "Fifteen", "Thirty", "Forty"]
-      s = p[@player1_score]
-      @player1_score == @player2_score ? s + "-All" : s + "-" + p[@player2_score]
+      s = p[game.player1_score]
+      game.player1_score == game.player2_score ? s + "-All" : s + "-" + p[game.player2_score]
     else
-      if (@player1_score == @player2_score)
+      if (game.player1_score == game.player2_score)
         "Deuce"
       else
-        s = @player1_score > @player2_score ? @player1_name : @player2_name
-        (@player1_score-@player2_score)*(@player1_score-@player2_score) == 1 ? "Advantage " + s : "Win for " + s
+        s = game.player1_score > game.player2_score ? game.player1_name : game.player2_name
+        (game.player1_score-game.player2_score)*(game.player1_score-game.player2_score) == 1 ? "Advantage " + s : "Win for " + s
       end
     end
   end
