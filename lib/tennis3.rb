eca62566393f7387a97416cc71d5c0777a82297e
jmks <jason.schweier@gmail.com>
2014-12-27 18:39:39 -0500

    refactored visitor's say_score method

--- lib/tennis3.rb
+++ lib/tennis3.rb
@@ -26,18 +26,49 @@ class TennisGame
 end
 
 class ScoreSayerVisitor
+  SCORES_IN_WORDS = ["Love", "Fifteen", "Thirty", "Forty"]
+
   def say_score game
-    if (game.player1_score < 4 and game.player2_score < 4) and (game.player1_score + game.player2_score < 6)
-      p = ["Love", "Fifteen", "Thirty", "Forty"]
-      s = p[game.player1_score]
-      game.player1_score == game.player2_score ? s + "-All" : s + "-" + p[game.player2_score]
+    early_game?(game) ? say_early_game_score(game) : say_late_game_score(game)
+  end
+
+  private
+
+  def say_early_game_score game
+    if tied?(game)
+      SCORES_IN_WORDS[game.player1_score] + "-All"
     else
-      if (game.player1_score == game.player2_score)
-        "Deuce"
-      else
-        s = game.player1_score > game.player2_score ? game.player1_name : game.player2_name
-        (game.player1_score-game.player2_score)*(game.player1_score-game.player2_score) == 1 ? "Advantage " + s : "Win for " + s
-      end
+      SCORES_IN_WORDS[game.player1_score] + "-" + SCORES_IN_WORDS[game.player2_score]
     end
+  end
+
+  def say_late_game_score game
+    if tied?(game)
+      "Deuce"
+    elsif advantage?(game)
+      "Advantage #{ leader_name(game) }"
+    else # Win
+      "Win for #{ leader_name(game) }"
+    end
+  end
+
+  def leader_name game
+    game.player1_score > game.player2_score ? game.player1_name : game.player2_name
+  end
+
+  def score_diff game
+    (game.player1_score - game.player2_score).abs
+  end
+
+  def tied? game
+    game.player1_score == game.player2_score
+  end
+
+  def early_game? game
+    (game.player1_score < 4 && game.player2_score < 4) && (game.player1_score + game.player2_score < 6)
+  end
+
+  def advantage? game
+    score_diff(game) == 1
   end
 end
